e5e6ae7e505f5434105853dc51c8410fa295c0f2
Alexander Schöberl <alexschoeberl@users.noreply.github.com>
2017-05-17 19:17:53 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,30 @@
 from os import path
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
 
 here = path.abspath(path.dirname(__file__))
+
+# copied from https://docs.pytest.org/en/latest/goodpractices.html
+class PyTest(TestCommand):
+	user_options = [('pytest-args=', 'a', "Arguments to pass to pytest")]
+
+	def initialize_options(self):
+		TestCommand.initialize_options(self)
+		self.pytest_args = []
+
+	def run_tests(self):
+		import shlex
+		#import here, cause outside the eggs aren't loaded
+		import pytest
+		errno = pytest.main(shlex.split(self.pytest_args))
+		sys.exit(errno)
+
 
 setup(
 	name='Pabiana',
 	version='0.0.1',
 	packages=find_packages(),
-	install_requires=['pyzmq']
+	install_requires=['pyzmq'],
+	tests_require=['pytest'],
+	cmdclass = {'test': PyTest}
 )

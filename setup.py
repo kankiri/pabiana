a3b3d5af83b48c7290883a53c28d3265f068fa20
Alexander Schöberl <alexschoeberl@users.noreply.github.com>
2017-05-19 14:38:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,11 @@ here = path.abspath(path.dirname(__file__))
 setup(
 	name='Pabiana',
 	version='0.0.1',
+	url='https://github.com/kankiri/pabiana',
+	author='Alexander Schöberl',
+	author_email='alexander.schoeberl@gmail.com',
+	description='A minimalistic framework to build distributed cognitive applications based on ØMQ',
+	long_description=open(os.path.join(ROOT, 'README.md')).read(),
 	packages=find_packages(),
 	install_requires=['pyzmq'],
 	setup_requires=['pytest-runner'],

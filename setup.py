6edabf416bf28dca75d6c0c54b5558c8006f6578
Alexander Schöberl <alexander.schoeberl@gmail.com>
2019-05-02 17:13:53 +0200

    Change to pathlib

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
-from os import path
+from pathlib import Path
 from setuptools import setup, find_packages
 
-here = path.abspath(path.dirname(__file__))
+root = Path(__file__).parent
+version = root / 'VERSION.txt'
+requirements = root / 'requirements.txt'
+readme = root / 'README.md'
 
 setup(
 	name='Pabiana',
-	version=open(path.join(here, 'VERSION.txt')).read().strip(),
+	version=version.read_text('utf-8').strip(),
 	packages=find_packages(),
-	install_requires=open(path.join(here, 'requirements.txt')).read().split('\n'),
+	install_requires=requirements.read_text('utf-8').split('\n'),
 	setup_requires=['pytest-runner >= 4.4'],
 	tests_require=['pytest >= 4.4.1'],
 	zip_safe=False,
@@ -16,7 +19,7 @@ setup(
 	author='Alexander Schöberl',
 	author_email='alexander.schoeberl@gmail.com',
 	description='A minimalistic framework to build distributed cognitive applications based on ØMQ',
-	long_description=open(path.join(here, 'README.md')).read(),
+	long_description=readme.read_text('utf-8'),
 	long_description_content_type='text/markdown',
 	keywords=['framework', 'cognitive', 'distributed', 'ØMQ'],
 	license='MIT'
